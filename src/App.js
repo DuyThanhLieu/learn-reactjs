fa3008c7944b68e8ce9b949a1ad7454c3265370d
Duy-Thanh_lieu <duythanhlieu2001@gmail.com>
2023-05-12 01:03:06 +0700

    thanh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="app">Learn ReactJS - Liêu Duy Thanh ^^</div>;
+  return <div className="app">Learn ReactJS - Liêu Duy Thanh Hihi! ^^</div>;
 }
 
 export default App;
